d0b3d18d266d6338b97d2b6299777161c3580ced
Claus Zimmermann <cz@impossiblesoftware.com>
2015-02-19 10:42:03 +0100

    Update Movie.proto (varlength ImageProvider)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ImpossibleVideo',
-    version='2.5.2',
+    version='2.6.0',
     author='Impossible Software GmbH',
     author_email='iv@impossiblesoftware.com',
     packages=['iv',],
